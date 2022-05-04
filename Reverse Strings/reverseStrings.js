e197a3c60ac9953d2df4df8e24e09ca90a7c32b5
jenniele913 <103193176+jenniele913@users.noreply.github.com>
2022-05-04 15:57:01 -0500

    reverses the string passed into it

---
+++ Reverse Strings/reverseStrings.js
@@ -0,0 +1,10 @@
+//Complete the solution so that it reverses the string passed into it
+
+function solution(str){
+    let word = str.split('').reverse().join('')
+    return word
+  }
+  
+  console.log(solution('hello'))
+  
+  
