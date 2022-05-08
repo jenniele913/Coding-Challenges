93ac996fefc837a3994b1520156cd9dee1753a11
jenniele913 <103193176+jenniele913@users.noreply.github.com>
2022-05-08 18:03:54 -0500

    convert name into intitials

---
+++ Abbreviate a Two Word Name/abbreviate2WordName.js
@@ -0,0 +1,13 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+function abbrevName(name){
+
+    return name.split(' ').map(name=>name[0]).join('.').toUpperCase()
+   
+   }
